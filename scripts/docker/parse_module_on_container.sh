7bdf597b511356ba0cf611a28e16235dc59d415b
Albert Albala <albert@mediatribe.net>
2025-11-23 11:21:07 -0500

    2025-11-23-11:21:06

--- scripts/docker/parse_module_on_container.sh
+++ scripts/docker/parse_module_on_container.sh
@@ -4,10 +4,10 @@
 #
 set -e
 
-CLEAN=$(echo "$1" | sed 's/\//_/g' | sed 's/:/_/g')
+CLEAN=$(echo "$1" | sed 's/\//_/g' | sed 's/:/_/g' | sed 's/\./_/g')
 
 cd /my/do-not-commit
 git clone --depth 1 "$1" "$CLEAN"
 cd -
 composer require phpmetrics/phpmetrics
-./vendor/bin/phpmetrics --report-html=myreport /my/do-not-commit/"$CLEAN" && mv myreport /my/docs/report-"$CLEAN"
+./vendor/bin/phpmetrics --report-html=myreport --report-json=myreportjson /my/do-not-commit/"$CLEAN" && mv myreport /my/docs/report-"$CLEAN" && mv myreportjson /my/docs/_data/json-"$CLEAN".json
--- scripts/run.sh
+++ scripts/run.sh
@@ -11,6 +11,7 @@ echo "OK"
 rm -rf do-not-commit
 mkdir do-not-commit
 rm -rf ./docs/report-*
+rm -rf ./docs/_data/json-*
 
 while read -r line; do
   if [[ "$line" == "- git_repo"* ]]; then
